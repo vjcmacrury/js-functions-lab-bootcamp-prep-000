68645c838c916f9eb87bc89f1a92fde83c2df095
vjcmacrury@gmail.com MacRury <vjcmacrury@gmail.com>
2018-05-19 17:35:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ function happyHolidaysTo(you) {
   return("Happy holidays, you!")
 }
 
-function happyHolidaysTo()
+function happyHolidaysTo(Independence Day, you) {
+  return("Happy Independence Day, you!")
+}
+
+function holidayCountdown()
